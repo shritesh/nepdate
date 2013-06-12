7bad7c2d39d05aaddf48557c33e3a6dc9256c316
Shritesh Bhattarai <shritesh@shritesh.com.np>
2013-06-12 20:39:12 +0545

    Converted to LGPL

--- nepdate.py
+++ nepdate.py
@@ -1,16 +1,16 @@
 ### BEGIN LICENSE
-# Copyright (C) 2011 Shritesh Bhattarai shriteshb@gmail.com
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3, as published 
+# Copyright (C) 2013 Shritesh Bhattarai shritesh@shritesh.com.np
+# This library is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU Lesser General Public License, as published 
 # by the Free Software Foundation.
 # 
-# This program is distributed in the hope that it will be useful, but 
+# This library is distributed in the hope that it will be useful, but 
 # WITHOUT ANY WARRANTY; without even the implied warranties of 
 # MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
-# PURPOSE.  See the GNU General Public License for more details.
+# PURPOSE.  See the GNU Lesser General Public License for more details.
 # 
-# You should have received a copy of the GNU General Public License along 
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
 import datetime
